eb094e3a8e65e7cbade1f60254b9a343b4228959
Guido Percu <guidopercu@gmail.com>
2025-03-12 13:50:46 -0400

    ADE-98 Infrastructure with ALB + Cognito

---
+++ backend/src/types/aws-cdk-lib.d.ts
@@ -0,0 +1,7 @@
+declare module 'aws-cdk-lib';
+declare module 'aws-cdk-lib/aws-ec2';
+declare module 'aws-cdk-lib/aws-ecs';
+declare module 'aws-cdk-lib/aws-ecs-patterns';
+declare module 'aws-cdk-lib/aws-apigateway';
+declare module 'aws-cdk-lib/aws-ecr';
+declare module 'constructs';
